30e9fbc9407a064cd8438e8a2a826c4db6d09ef7
Andrew Ahr <andrew.ahr51@gmail.com>
2020-04-12 19:59:03 -0600

    Added call to API and fixed filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from  '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { StarComponent } from './shared/star.component';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import { FilterPipe} from './shared/filterpipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, FilterPipe, StarComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  bootstrap: [AppComponent],
+
 })
-export class AppModule { }
+export class AppModule {}

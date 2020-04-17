2119f7d787129fa2c37f2f62cef7ce5a94ed71c5
Andrew Ahr <andrew.ahr51@gmail.com>
2020-04-17 08:05:21 -0600

    Final First Phase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { CustomerComponent } from './customer/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
